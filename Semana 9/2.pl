c96f0b11cf4fb6faa7253bc12d9c4a047085e710
Justin Alonso Martínez Molina <127434581+justinalonsomm@users.noreply.github.com>
2024-04-11 22:06:47 -0600

    Delete Semana 9 directory

--- Semana 9/2.pl
+++
@@ -1,9 +0,0 @@
-aplanar([], []).
-aplanar([X|Xs], L) :-
-    is_list(X),
-    aplanar(X, L1),
-    aplanar(Xs, L2),
-    append(L1, L2, L).
-aplanar([X|Xs], [X|L]) :-
-    \+ is_list(X),
-    aplanar(Xs, L).
